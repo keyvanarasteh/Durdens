bd304450b06d546ac4dbabf81363e9d324def91f
M-Ikrime <myagbasa120@gmail.com>
2022-12-27 14:48:08 +0300

    some

---
+++ project/durdens_1/screens/carDetailScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class carDetailScreen extends StatelessWidget {
+  const carDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
