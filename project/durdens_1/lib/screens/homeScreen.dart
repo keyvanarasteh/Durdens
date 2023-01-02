a3124c7030b4cb16b701ce93d150c1091e11b922
erencicekk25 <eerencicekk1@gmail.com>
2023-01-03 01:08:27 +0300

    some

--- project/durdens_1/lib/screens/homeScreen.dart
+++ project/durdens_1/lib/screens/homeScreen.dart
@@ -17,26 +17,14 @@ class _nameState extends State<HomeScreen> {
     return Scaffold(
       // ignore: duplicate_ignore
       appBar: AppBar(
-          actions: [
+          actions: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: IconButton(
-                  icon: Icon(
-                    Icons.person_outline,
-                    color: Colors.black,
-                    size: 30,
-                  ),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text('Profile'),
-                            content: const Text('Profilim'),
-                          );
-                        });
-                  }),
-            )
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                radius: 20,
+                backgroundImage: AssetImage('images/profile.png'),
+              ),
+            ),
           ],
           leading: Icon(
             Icons.menu,
@@ -52,68 +40,378 @@ class _nameState extends State<HomeScreen> {
           backgroundColor: Colors.yellow),
       // ignore: avoid_unnecessary_containers
       body: Column(children: [
-        Container(
-          height: 50,
-          width: 300,
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: 50,
+            width: 300,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: 'Arama yapın...',
+                prefixIcon: Icon(Icons.search),
               ),
-              hintText: 'Arama yapın...',
-              prefixIcon: Icon(Icons.search),
+              onChanged: (value) {
+                // Kullanıcının girdiği değer burada işlenir
+              },
             ),
-            onChanged: (value) {
-              // Kullanıcının girdiği değer burada işlenir
-            },
           ),
         ),
-        Column(
-          children: [
-            Container(
-              child: Row(children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                  child: Text(
-                    "Acil",
+        Expanded(
+          flex: 1,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [Icon(Icons.warning_outlined), Text("Acil")],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Colors.red, //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
                   ),
-                )
-              ]),
-            )
-          ],
-        ),
-        Container(
-          child: Row(children: [
-            Expanded(
-                child: Container(
-              margin: const EdgeInsets.all(10.0),
-              height: 70,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 187, 190, 195),
-                borderRadius: BorderRadius.circular(12),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [Icon(Icons.warning_outlined), Text("Acil")],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Colors.red, //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [Icon(Icons.warning_outlined), Text("Acil")],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Colors.red, //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [Icon(Icons.warning_outlined), Text("Acil")],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Colors.red, //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [Icon(Icons.warning_outlined), Text("Acil")],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Colors.red, //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [Icon(Icons.warning_outlined), Text("Acil")],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Colors.red, //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [
+                        Icon(Icons.move_down),
+                        Text("Fiyatı düşenler")
+                      ],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(
+                            255, 8, 233, 180), //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: Center(
+                        child: Column(
+                      children: [
+                        Icon(Icons.punch_clock_outlined),
+                        Text("Son 48 saat")
+                      ],
+                    )),
+                    margin: const EdgeInsets.all(
+                        10), //Dışarıya müdahale eder (Kenarlara Boşluk)
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(
+                            255, 63, 143, 209), //rengini değiştirir
+                        // ignore: prefer_const_literals_to_create_immutables
+                        shape: BoxShape.rectangle, //şekili daire yapar
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                ],
               ),
-              child: Row(children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Icon(
-                    Icons.home,
-                    size: 50,
-                  ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Row(children: [
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.all(10.0),
+                height: 60,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 232, 234, 237),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 25),
-                  child: Text(
-                    "Emlak",
-                    style: TextStyle(fontSize: 25),
+                child: Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(
+                      color: Color.fromARGB(255, 0, 154, 237),
+                      Icons.home,
+                      size: 50,
+                    ),
                   ),
-                )
-              ]),
-            ))
-          ]),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Emlak",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color.fromARGB(255, 0, 154, 237)),
+                        ),
+                      ) //Kendimize öz
+                      )
+                ]),
+              )),
+            ]),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Row(children: [
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.all(10.0),
+                height: 60,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 232, 234, 237),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(
+                      color: Color.fromARGB(255, 146, 0, 237),
+                      Icons.car_rental,
+                      size: 50,
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Vasıta",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color.fromARGB(255, 146, 0, 237)),
+                        ),
+                      ) //Kendimize öz
+                      )
+                ]),
+              )),
+            ]),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Row(children: [
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.all(10.0),
+                height: 60,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 232, 234, 237),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(
+                      color: Color.fromARGB(255, 1, 251, 201),
+                      Icons.key_outlined,
+                      size: 50,
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Yedek Parça",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color.fromARGB(255, 1, 251, 201)),
+                        ),
+                      ) //Kendimize öz
+                      )
+                ]),
+              )),
+            ]),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Row(children: [
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.all(10.0),
+                height: 60,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 232, 234, 237),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(
+                      color: Color.fromARGB(255, 251, 218, 1),
+                      Icons.back_hand_outlined,
+                      size: 50,
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "İkinci El",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color.fromARGB(255, 251, 218, 1)),
+                        ),
+                      ) //Kendimize öz
+                      )
+                ]),
+              )),
+            ]),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Row(children: [
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.all(10.0),
+                height: 60,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 232, 234, 237),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(
+                      color: Color.fromARGB(255, 1, 122, 251),
+                      Icons.business,
+                      size: 50,
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "İş ilanları",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Color.fromARGB(255, 1, 122, 251)),
+                        ),
+                      ) //Kendimize özel custom bir button yaptık
+                      )
+                ]),
+              )),
+            ]),
+          ),
         ),
       ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),
     );
   }
 }
